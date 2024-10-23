f6c679a9666678a695193dfac0ce5a4840a875e7
Turtel216 <papadimitrios216@gmail.com>
2024-10-24 00:56:32 +0300

    Added values to parser

--- interpreter/app/Main.hs
+++ interpreter/app/Main.hs
@@ -1,10 +1,40 @@
 module Main where
 
+import Control.Monad (liftM)
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+-- Data type holding any Scheme Value
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
 
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
