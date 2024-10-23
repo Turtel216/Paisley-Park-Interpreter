079374ecd47d37f73be79217c8217b263825b342
Turtel216 <papadimitrios216@gmail.com>
2024-10-24 00:51:08 +0300

    Added basic parser

--- interpreter/app/Main.hs
+++ interpreter/app/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
 main :: IO ()
-main = putStrLn "Hello, Scheme!"
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
