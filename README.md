4accd3c3d04664c0631776a290f7991db9e5c243
Turtel216 <papadimitrios216@gmail.com>
2024-10-25 11:07:25 +0300

    Refactored Project

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 ## About 
 
-An interpreter for most of the [The Scheme Programming Language](https://www.scheme.org/), based on the book [Write Yourself a Scheme in 48 Hours](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours). The main purpose of the project is to get a better understanding of building applications in Haskell
+An interpreter for most of the [The Scheme Programming Language](https://www.scheme.org/), based on the book [Write You A Scheme, Version 2.0](https://wespiser.com/writings/wyas/00_overview.html). The main purpose of the project is to get a better understanding of building applications in Haskell
---
+++ interpreter/app/Parser.hs
@@ -0,0 +1,72 @@
+module Parser where
+
+import Control.Monad (liftM)
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+-- Data type holding any Scheme Value
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseString :: Parser LispVal
+parseString = do
+  _ <- char '"'
+  x <- many (noneOf "\"")
+  _ <- char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  _ <- char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
+parseExpr :: Parser LispVal
+parseExpr =
+  parseAtom
+    <|> parseString
+    <|> parseNumber
+    <|> parseQuoted
+    <|> do
+      _ <- char '('
+      x <- try parseList <|> parseDottedList
+      _ <- char ')'
+      return x
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right _ -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
